d6d3a42d7d19e046a27f137b1de41c1f13f00610
May Han <may.zhe.han@gmail.com>
2025-10-19 22:38:15 +0800

    chore(ci): relax commitlint limits to reduce false negatives\n\n- header-max-length 120\n- body/footer line length 200

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Relax limits to reduce noisy CI failures while keeping basic hygiene
+    'header-max-length': [2, 'always', 120],
+    'body-max-line-length': [1, 'always', 200],
+    'footer-max-line-length': [1, 'always', 200],
+  },
+};
