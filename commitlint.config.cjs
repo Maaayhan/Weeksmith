4606fe8785c46e5c8413469b013cbb1831cc78c8
May Han <may.zhe.han@gmail.com>
2025-10-19 22:41:51 +0800

    chore(ci): temporarily disable header length check in commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,7 +2,8 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     // Relax limits to reduce noisy CI failures while keeping basic hygiene
-    'header-max-length': [2, 'always', 120],
+    // Disable header length enforcement for now (long titles allowed)
+    'header-max-length': [0, 'always', 120],
     'body-max-line-length': [1, 'always', 200],
     'footer-max-line-length': [1, 'always', 200],
   },
